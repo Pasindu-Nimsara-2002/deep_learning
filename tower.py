e648dbfcd7335ea8055bbb2de8fa7e1f3925ea38
Pasindu Nimsara <pasindunk2002@gmail.com>
2025-02-27 12:51:31 +0800

    tower

--- tower.py
+++ tower.py
@@ -1,2 +1,9 @@
-def tower(a,b,c,n):
-    return 
+def tower(start, target, extra, n):
+    if n == 1:
+        print("Move 1 from", start, "to", target)
+    else:
+        tower(start, extra, target, n-1)
+        print("Move", n, "from", start, "to", target)
+        tower(extra, target, start, n-1)    
+
+tower("A", "B", "C", 5)        
