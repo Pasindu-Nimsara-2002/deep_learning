ca0181131dc8566b0c62f2a68cf402c6e9a62267
Pasindu Nimsara <pasindunk2002@gmail.com>
2025-02-27 12:54:02 +0800

    tower_v01

--- tower.py
+++ tower.py
@@ -1,6 +1,7 @@
 def tower(start, target, extra, n):
     if n == 1:
         print("Move 1 from", start, "to", target)
+        return
     else:
         tower(start, extra, target, n-1)
         print("Move", n, "from", start, "to", target)
